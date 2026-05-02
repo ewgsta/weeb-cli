289b0f6b1c8e57ff6d29c8fe3f8b7945085b10e7
ewgsta <159681870+ewgsta@users.noreply.github.com>
2026-05-02 16:42:01 +0300

    chore: bump version v2.16.1

--- weeb_cli/__init__.py
+++ weeb_cli/__init__.py
@@ -31,7 +31,7 @@ Attributes:
     __version__ (str): Current version of Weeb CLI.
 """
 
-__version__ = "2.16.0"
+__version__ = "2.16.1"
 
 # SDK exports for easy import
 from weeb_cli.sdk import WeebSDK, list_providers, get_provider_info
